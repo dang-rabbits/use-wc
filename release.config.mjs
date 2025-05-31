cdfc4d58be3dc5f0d20a669bf9d2071a47b0f46d
Zach Johnson <hellozachj@gmail.com>
2025-05-31 11:59:22 -0700

    fix: invalid release branch config

--- release.config.mjs
+++ release.config.mjs
@@ -1,3 +1,11 @@
 export default {
-  branches: ['1.x.x', { name: 'main', channel: 'pre', prerelease: true }],
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    { name: 'main', channel: 'pre', prerelease: true },
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
 };
