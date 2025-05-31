bf983e5ce87ea6f143ee7e0784f944dc89be8026
Zach Johnson <hellozachj@gmail.com>
2025-05-31 09:49:20 -0700

    fix: release config format

---
+++ release.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  branches: [
+    { name: 'main' },
+    { name: 'alpha', channel: 'alpha', prerelease: true },
+    { name: 'beta', channel: 'beta', prerelease: true },
+    { name: 'next', channel: 'next' },
+  ],
+};
